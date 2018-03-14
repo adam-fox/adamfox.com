8cb21218f6396db5281bfc740da386739b38b085
Adam Fox <adam@adams-mbp.lan>
2018-03-14 16:42:50 -0500

    responsive frames development.

--- js/adamfox.js
+++ js/adamfox.js
@@ -75,6 +75,220 @@ $(window).scroll(function() {
 	}
 });
 
+
+
+// init controller
+/*
+var controller = new ScrollMagic.Controller({globalSceneOptions: {duration: 100}});
+
+$(".expertise > ul > li > div").each(function (index, elem) {
+	
+	var myScene = new ScrollMagic.Scene({
+        triggerElement: this,
+        duration: 2000,
+        triggerHook: "onEnter"
+    })
+    
+	.setClassToggle(this, 'in-view')
+// 	.removeClassToggle(false)
+		.addIndicators()
+	    .addTo(controller);
+	
+});
+*/
+
+/*
+$(".history > ul > li").each(function (index, elem) {
+	new ScrollMagic.Scene({triggerElement:elem, triggerHook: "onEnter"})
+	.setClassToggle(elem, "line-animation") // add class toggle
+	.addIndicators() // add indicators (requires plugin)
+	.addTo(controller);
+});
+*/
+
+$("header > .wrapper > .logo_af").click(function() {
+	$("html, body").animate({ scrollTop: "0px" });
+});
+
+
+$(".btn_menu").click(function() {
+	$(this).toggleClass('on');
+	$('nav > ul').toggleClass('on');
+	$('.logo_af').toggleClass('hidden');
+	$('.logo_circle').toggleClass('hidden');
+});
+
+// **********
+// TOUCHSWIPE
+// **********
+
+$(function() {      
+	//Enable swiping...
+	$(".browser").swipe( {
+		swipeLeft:function(event, direction, distance, duration, fingerCount) {
+			var browser = $(this);
+			var paginationNext = browser.parent().children(".pagination").children(".on");
+			if(browser.parent().children(".pagination").children("a").last().hasClass("on")){
+				browser.css("transform","translate( -100vw ,0)");
+				browser.parent().children(".pagination").children(".on").removeClass("on");
+				browser.parent().children(".pagination").children("a").first().addClass("on");
+				setTimeout(function() {
+					browser.css("transition","none");
+					browser.css("transform","translate( 100vw ,0)");
+			    }, 250);
+			    setTimeout(function() {
+					browser.find(".viewport > img.desktop").css("left", "0%");
+					browser.find(".viewport > img.tablet").css("left", "0%");
+					browser.find(".viewport > img.mobile").css("left", "0%");
+					browser.css("transition","all .25s linear");
+					browser.css("transform","translate(0,0)");
+			    }, 350);
+			} else {
+				paginationNext.next().trigger("click");
+			}
+			
+		},
+		swipeRight:function(event, direction, distance, duration, fingerCount) {
+			var browser = $(this);
+			var paginationPrev = browser.parent().children(".pagination").children(".on").prev();
+			if(browser.parent().children(".pagination").children("a").first().hasClass("on")){
+				browser.css("transform","translate( 100vw ,0)");
+				browser.parent().children(".pagination").children(".on").removeClass("on");
+				browser.parent().children(".pagination").children("a").last().addClass("on");
+				setTimeout(function() {
+					browser.css("transition","none");
+					browser.css("transform","translate( -100vw ,0)");
+			    }, 250);
+			    setTimeout(function() {
+					browser.find(".viewport > img.desktop").css("left", "-" + browser.parent().children(".pagination").children("a").last().index() * 100 + "%");
+					browser.find(".viewport > img.tablet").css("left", "-" + browser.parent().children(".pagination").children("a").last().index() * 100 + "%");
+					browser.find(".viewport > img.mobile").css("left", "-" + browser.parent().children(".pagination").children("a").last().index() * 100 + "%");
+					browser.css("transition","all .25s linear");
+					browser.css("transform","translate(0,0)");
+			    }, 350);
+			} else {
+				paginationPrev.trigger("click");
+			}
+		},
+		threshold:25
+	});
+});
+
+
+$(".pagination > a").click(function() {
+	var paginationClicked = $(this);
+	var paginationOn = $(this).parent().find('.on');
+	
+	if(!$(this).hasClass("on")) {
+		
+		if(paginationClicked.index() > paginationOn.index()) {
+			paginationClicked.parent().parent().children(".browser").css("transform","translate( -100vw ,0)");
+			paginationOn.removeClass("on");
+			paginationClicked.addClass("on");
+			setTimeout(function() {
+				paginationClicked.parent().parent().children(".browser").css("transition","none");
+				paginationClicked.parent().parent().children(".browser").css("transform","translate( 100vw ,0)");
+				
+		    }, 250);
+		} else {
+			paginationClicked.parent().parent().children(".browser").css("transform","translate( 100vw ,0)");
+			paginationOn.removeClass("on");
+			paginationClicked.addClass("on");
+			setTimeout(function() {
+				paginationClicked.parent().parent().children(".browser").css("transition","none");
+				paginationClicked.parent().parent().children(".browser").css("transform","translate( -100vw ,0)");
+		    }, 250);
+		}
+		setTimeout(function() {
+			paginationClicked.parent().parent().find(".browser > .viewport > img.desktop").css("left", "-" + paginationClicked.index() * 100 + "%");
+			paginationClicked.parent().parent().find(".browser > .viewport > img.tablet").css("left", "-" + paginationClicked.index() * 100 + "%");
+			paginationClicked.parent().parent().find(".browser > .viewport > img.mobile").css("left", "-" + paginationClicked.index() * 100 + "%");
+			paginationClicked.parent().parent().children(".browser").css("transition","all .25s linear");
+			paginationClicked.parent().parent().children(".browser").css("transform","translate(0,0)");
+	    }, 350);
+	}	
+
+});
+
+
+
+$(".main").click(function() {
+	$('html, body').animate({ scrollTop: $("#experience").offset().top - 120 }, 2000);
+});
+
+var animationEvent = 'webkitAnimationEnd oanimationend msAnimationEnd animationend';
+
+$(".btn").click(function () {
+	if(!$(this).hasClass("disabled")) {
+		$(this).addClass('on');
+		$(this).one(animationEvent, function(event) {
+			$(this).removeClass('on');
+		});
+	}
+});
+
+$(".plus").click(function() {
+	
+	if(!$(this).hasClass("disabled")) {
+		$(this).parent().children(".minus").removeClass("disabled");
+		if($(this).parent().parent().parent().hasClass("mobile")) {
+			if($(this).parent().parent().parent().find(".browser > .viewport > img.tablet")[0]) {
+				$(this).parent().parent().parent().removeClass("mobile");
+				$(this).parent().parent().parent().addClass("tablet");
+				$(this).parent().children("span").text("Tablet");
+				
+				if(!$(this).parent().parent().parent().find(".browser > .viewport > img.desktop")[0]) {
+					$(this).addClass("disabled");
+				}
+			} else if($(this).parent().parent().parent().find(".browser > .viewport > img.desktop")[0]) {
+				$(this).parent().parent().parent().removeClass("mobile");
+				$(this).parent().parent().parent().addClass("desktop");
+				$(this).parent().children("span").text("Desktop");
+				$(this).addClass("disabled");
+			}
+		} else if($(this).parent().parent().parent().hasClass("tablet")) {
+			if($(this).parent().parent().parent().find(".browser > .viewport > img.desktop")[0]) {
+				$(this).parent().parent().parent().removeClass("tablet");
+				$(this).parent().parent().parent().addClass("desktop");
+				$(this).parent().children("span").text("Desktop");
+				$(this).addClass("disabled");
+			}
+		}
+	}
+});
+
+$(".minus").click(function() {
+	if(!$(this).hasClass("disabled")) {
+		$(this).parent().children(".plus").removeClass("disabled");
+		if($(this).parent().parent().parent().hasClass("tablet")) {
+			if($(this).parent().parent().parent().find(".browser > .viewport > img.mobile")[0]) {
+				$(this).parent().parent().parent().removeClass("tablet");
+				$(this).parent().parent().parent().addClass("mobile");
+				$(this).parent().children("span").text("Mobile");
+				$(this).addClass("disabled");
+			}
+		} else if($(this).parent().parent().parent().hasClass("mobile")) {
+		} else {
+			if($(this).parent().parent().parent().find(".browser > .viewport > img.tablet")[0]) {
+				$(this).parent().parent().parent().removeClass("desktop");
+				$(this).parent().parent().parent().addClass("tablet");
+				$(this).parent().children("span").text("Tablet");
+				
+				if(!$(this).parent().parent().parent().find(".browser > .viewport > img.mobile")[0]) {
+					$(this).addClass("disabled");
+				}
+			} else if($(this).parent().parent().parent().find(".browser > .viewport > img.mobile")[0]) {
+				$(this).parent().parent().parent().removeClass("desktop");
+				$(this).parent().parent().parent().addClass("mobile");
+				$(this).parent().children("span").text("Mobile");
+				$(this).addClass("disabled");
+			}
+		}
+	}
+});
+
+
+
 /*
 $(document).ready(function () {
     var classes = new Array ('desktop', 'mobile', 'tablet');
@@ -95,173 +309,6 @@ $(document).ready(function () {
         } else {
 	        $(this).parent().find("span").text("Tablet");
         }
-	});
-});
-*/
-
-$("header > .wrapper > .logo_af").click(function() {
-	$("html, body").animate({ scrollTop: "0px" });
-});
-
-
-$(".btn_menu").click(function() {
-	$(this).toggleClass('on');
-	$('nav > ul').toggleClass('on');
-	$('.logo_af').toggleClass('hidden');
-	$('.logo_circle').toggleClass('hidden');
-});
-
-$(function() {      
-	//Enable swiping...
-	$(".browser").swipe( {
-		swipeLeft:function(event, direction, distance, duration, fingerCount) {
-			var browser = $(this);
-			browser.css("transform","translate( -" + (window.innerWidth + 300) / 2 + "px ,0)");
-			browser.css("opacity","0");
-			var paginationPrev = browser.parent().children(".pagination").children(".on");
-			if(browser.parent().children(".pagination").children("a").last().hasClass("on")){
-				browser.parent().children(".pagination").children(".on").removeClass("on");
-				browser.parent().children(".pagination").children("a").first().addClass("on");
-			} else {
-				browser.parent().children(".pagination").children(".on").removeClass("on");
-				paginationPrev.next().addClass("on");
-				
-			}
-			setTimeout(function() {
-				browser.css("transition","none");
-				browser.css("transform","translate( " + (window.innerWidth + 300) / 2 + "px ,0)");
-		    }, 250);
-			setTimeout(function() {
-				browser.css("transition","all .25s linear");
-				browser.css("opacity","1");
-				browser.css("transform","translate(0,0)");
-		    }, 350);  
-		},
-		swipeRight:function(event, direction, distance, duration, fingerCount) {
-			var browser = $(this);
-			browser.css("transform","translate( " + (window.innerWidth + 300) / 2 + "px ,0)");
-			browser.css("opacity","0");
-			var paginationNext = browser.parent().children(".pagination").children(".on");
-			if(browser.parent().children(".pagination").children("a").first().hasClass("on")){
-				browser.parent().children(".pagination").children(".on").removeClass("on");
-				browser.parent().children(".pagination").children("a").last().addClass("on");
-			} else {
-				browser.parent().children(".pagination").children(".on").removeClass("on");
-				paginationNext.prev().addClass("on");
-				
-			}
-			setTimeout(function() {
-				browser.css("transition","none");
-				browser.css("transform","translate( -" + (window.innerWidth + 300) / 2 + "px ,0)");
-		    }, 250);
-			setTimeout(function() {
-				browser.css("transition","all .25s linear");
-				browser.css("opacity","1");
-				browser.css("transform","translate(0,0)");
-		    }, 350);  
-		},
-		threshold:25
-	});
-});
-
-
-$(".pagination > a").click(function() {
-	var pagination = $(this);
-	
-	if($(this).hasClass("on")) {
-		
-	} else if($(this).prevAll().hasClass("on")) {
-		pagination.parent().parent().children(".browser").css("transform","translate( -" + (window.innerWidth + 300) / 2 + "px ,0)");
-		pagination.parent().parent().children(".browser").css("opacity","0");
-		pagination.siblings().removeClass("on");
-		pagination.addClass("on");
-		setTimeout(function() {
-			pagination.parent().parent().children(".browser").css("transition","none");
-			pagination.parent().parent().children(".browser").css("transform","translate( " + (window.innerWidth + 300) / 2 + "px ,0)");
-	    }, 250);
-	} else {
-		pagination.parent().parent().children(".browser").css("transform","translate( " + (window.innerWidth + 300) / 2 + "px ,0)");
-		pagination.parent().parent().children(".browser").css("opacity","0");
-		pagination.siblings().removeClass("on");
-		pagination.addClass("on");
-		setTimeout(function() {
-			pagination.parent().parent().children(".browser").css("transition","none");
-			pagination.parent().parent().children(".browser").css("transform","translate( -" + (window.innerWidth + 300) / 2 + "px ,0)");
-	    }, 250);
-	}
-	setTimeout(function() {
-		pagination.parent().parent().children(".browser").css("transition","all .25s linear");
-		pagination.parent().parent().children(".browser").css("opacity","1");
-		pagination.parent().parent().children(".browser").css("transform","translate(0,0)");
-    }, 350);
-});
-
-
-/*
-$(".browser:odd").click(function() {
-	var browser = $(this);
-	browser.css("transform","translate( " + (window.innerWidth + 300) / 2 + "px ,0)");
-	setTimeout(function() {
-		browser.children('img').hide();
-		browser.css("transform","translate(0,0)");
-    }, 350);
-});
-*/
-
-
-$(".main").click(function() {
-	$('html, body').animate({ scrollTop: $("#experience").offset().top - 120 }, 2000);
-});
-
-var animationEvent = 'webkitAnimationEnd oanimationend msAnimationEnd animationend';
-
-$(".btn").click(function () {
-	if(!$(this).hasClass("disabled")) {
-		$(this).addClass('on');
-		$(this).one(animationEvent, function(event) {
-			$(this).removeClass('on');
-		});
-	}
-});
-
-$(".plus").click(function() {
-	$(this).parent().children(".minus").removeClass("disabled");
-	if($(this).parent().parent().parent().hasClass("mobile")) {
-		$(this).parent().parent().parent().removeClass("mobile");
-		$(this).parent().parent().parent().addClass("tablet");
-		$(this).parent().children("span").text("Tablet");
-	} else if($(this).parent().parent().parent().hasClass("tablet")) {
-		$(this).parent().parent().parent().removeClass("tablet");
-		$(this).parent().parent().parent().addClass("desktop");
-		$(this).parent().children("span").text("Desktop");
-		$(this).addClass("disabled");
-	}
-});
-
-$(".minus").click(function() {
-	$(this).parent().children(".plus").removeClass("disabled");
-	if($(this).parent().parent().parent().hasClass("tablet")) {
-		$(this).parent().parent().parent().removeClass("tablet");
-		$(this).parent().parent().parent().addClass("mobile");
-		$(this).parent().children("span").text("Mobile");
-		$(this).addClass("disabled");
-	} else if($(this).parent().parent().parent().hasClass("mobile")) {
-	} else {
-		$(this).parent().parent().parent().removeClass("desktop");
-		$(this).parent().parent().parent().addClass("tablet");
-		$(this).parent().children("span").text("Tablet");
-	}
-});
-
-/*
- $(function(){
-	$('.browser').okshadow({
-		color: 'rgba(0,0,0,.5)',
-		xMax: 2000,
-		yMax: 2000,
-		yOffset: 100,
-		fuzzMin: 500,
-		fuzzMax: 500,
 	});
 });
 */
